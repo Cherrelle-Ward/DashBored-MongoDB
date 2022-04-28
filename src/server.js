9ebd2ed9aaf4ca2b643216d96fbc23b71d507840
Cherrelle-Ward <Cherrelle_Ward@hotmail.co.uk>
2022-04-28 10:35:05 +0100

    added cloudinary fetch requests

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 require("./db/connection");
+const { cloudinary } = require("./utils/cloudinary");
 const express = require("express");
-const cors = require("cors");
-
+var cors = require("cors");
 const userRouter = require("./user/routes");
 
 //Declaring app using the express method
@@ -16,6 +16,37 @@ const port = process.env.PORT || 5001;
 app.use(express.json());
 app.use(cors());
 app.use(userRouter);
+
+// !cloudinary limits on files
+// app.use(express.static("public"));
+// app.use(express.json({ limit: "100mb" }));
+// app.use(express.urlencoded({ limit: "100mb", extended: true }));
+
+// !cloudinary api
+
+app.get("/api/images", async (req, res) => {
+  const { resources } = await cloudinary.search
+    .expression("folder:DashBored")
+    .sort_by("public_id", "desc")
+    .max_results(30)
+    .execute();
+
+  const publicIds = resources.map((file) => file.public_id);
+  res.send(publicIds);
+});
+app.post("/api/upload", async (req, res) => {
+  try {
+    const fileStr = req.body.data;
+    const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+      upload_preset: "ml_default",
+    });
+    console.log(uploadResponse);
+    res.json({ msg: "yaya" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err: "Something went wrong server side" });
+  }
+});
 
 //This is the listen. We only add the function so that we can add a console.log to see if it is working
 app.listen(port, () => {
